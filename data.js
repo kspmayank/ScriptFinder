e9a37fb393624be765bb02d3ab30ef535faf4d61
Mayank Kashyap <mayankkashyap95@gmail.com>
2020-01-25 15:40:05 +0530

    Initial Commit with Markup

---
+++ data.js
@@ -0,0 +1,58 @@
+data = [
+  {
+    name: "Script to find recently changed tickets",
+    metaAttributes: {
+      labels: ['label1', 'label2'],
+      owner: 'Ghouse',
+      lastUpdated: '02/23/2017',
+      createdBy: 'Shah',
+      createdOn: '07/31/2017',
+      source: 'C:/Users/Deskptop/urna_leo_convallis.py',
+      gitlabLink: 'https://www.google.com',
+      openGrokLink: 'https://www.google.com'
+    },
+    isFavourite: true
+  },
+  {
+    name: "List Item 2",
+    metaAttributes: {
+      labels: ['label1', 'label2'],
+      owner: 'Ghouse',
+      lastUpdated: '02/23/2017',
+      createdBy: 'Shah',
+      createdOn: '07/31/2017',
+      source: 'C:/Users/Deskptop/urna_leo_convallis.py',
+      gitlabLink: 'https://www.google.com',
+      openGrokLink: 'https://www.google.com'
+    },
+    isFavourite: false
+  },
+  {
+    name: "List Item 2",
+    metaAttributes: {
+      labels: ['label1', 'label2'],
+      owner: 'Ghouse',
+      lastUpdated: '02/23/2017',
+      createdBy: 'Shah',
+      createdOn: '07/31/2017',
+      source: 'C:/Users/Deskptop/urna_leo_convallis.py',
+      gitlabLink: 'https://www.google.com',
+      openGrokLink: 'https://www.google.com'
+    },
+    isFavourite: true
+  },
+  {
+    name: "List Item 3",
+    metaAttributes: {
+      labels: ['label1', 'label2'],
+      owner: 'Ghouse',
+      lastUpdated: '02/23/2017',
+      createdBy: 'Shah',
+      createdOn: '07/31/2017',
+      source: 'C:/Users/Deskptop/urna_leo_convallis.py',
+      gitlabLink: 'https://www.google.com',
+      openGrokLink: 'https://www.google.com'
+    },
+    isFavourite: false
+  }
+]
